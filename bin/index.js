a3261ab383af0adac654702a3737a47e1cf3f18e
pengtao <pengtao_it@163.com>
2021-07-02 17:16:32 +0800

    first commit

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const { program } = require('commander')
+
+program.version(require('../package.json').version, '-v, --versions', '当前版本号')
+
+program
+  .command('create <project-name>')
+  .description('创建项目')
+  .option('-g, --git', '是否使用git管理版本', true)
+  .option('--no-g, --no-git', '禁止使用git管理版本')
+  .option('-f, --force', '强制覆盖目录')
+  .action((name, options) => {
+    require('../lib/create')(name, options)
+  })
+
+// 解析参数
+program.parse(process.argv)
